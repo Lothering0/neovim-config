1ddbb8a59ce5121686ae548fa267a78147ec2499
Lothering0 <danil2005232@gmail.com>
2023-01-10 21:32:31 +0300

    Adding extra space and moving cursor on one position left after pressing space between parentheses/braces/etc

--- lua/editor/mappings.lua
+++ lua/editor/mappings.lua
@@ -1,5 +1,6 @@
 local helpers = require("helpers")
 local map = helpers.map
+local is_surrounded = helpers.is_surrounded
 
 -- Center the screen after jump on last line of file
 map("n", "G", "Gzz")
@@ -16,10 +17,30 @@ map("i", "(", "()<left>")
 map("i", "[", "[]<left>")
 map("i", "{", "{}<left>")
 
-local condition1 = 'getline(".")[col(".")-2:col(".")-1]=="{}" ? "<cr><esc>O"'
-local condition2 = 'getline(".")[col(".")-2:col(".")-1]=="()" ? "<cr><esc>O<tab>"'
-local condition3 = 'getline(".")[col(".")-2:col(".")-1]=="[]" ? "<cr><esc>O"'
-local condition4 = 'getline(".")[col(".")-2:col(".")-1]=="<>" ? "<cr><esc>O<tab>"'
-local conditions = condition1 .. ' : ' .. condition2 .. ' : ' .. condition3 .. ' : ' .. condition4
-local expression = conditions .. ' : "<cr>"'
-map("i", "<CR>", expression, true, true)
+-- Enter
+local enter_map_condition_1 = is_surrounded("{}") .. ' ? "<cr><esc>O"'
+local enter_map_condition_2 = is_surrounded("()") .. ' ? "<cr><esc>O<tab>"'
+local enter_map_condition_3 = is_surrounded("[]") .. ' ? "<cr><esc>O"'
+local enter_map_condition_4 = is_surrounded("<>") .. ' ? "<cr><esc>O<tab>"'
+local enter_map_conditions = table.concat({
+  enter_map_condition_1,
+  enter_map_condition_2,
+  enter_map_condition_3,
+  enter_map_condition_4
+}, " : ")
+local enter_map_expression = enter_map_conditions .. ' : "<cr>"'
+map("i", "<CR>", enter_map_expression, true, true)
+
+-- Space
+local space_map_condition_1 = is_surrounded("{}") .. ' ? "<Space><Space><Left>"'
+local space_map_condition_2 = is_surrounded("[]") .. ' ? "<Space><Space><Left>"'
+local space_map_condition_3 = is_surrounded("()") .. ' ? "<Space><Space><Left>"'
+local space_map_condition_4 = is_surrounded("<>") .. ' ? "<Space><Space><Left>"'
+local space_map_conditions = table.concat({
+  space_map_condition_1,
+  space_map_condition_2,
+  space_map_condition_3,
+  space_map_condition_4
+}, " : ")
+local space_map_expression = space_map_conditions .. ' : "<Space>"'
+map("i", "<Space>", space_map_expression, true, true)
--- lua/helpers.lua
+++ lua/helpers.lua
@@ -2,7 +2,7 @@ local g = vim.g
 local o = vim.opt
 local c = vim.cmd
 
-function map(mode, key, v, noremap, expr)
+local function map(mode, key, v, noremap, expr)
   local noremap = noremap or true
   vim.api.nvim_set_keymap(mode, key, v, {
     silent = true,
@@ -14,11 +14,50 @@ end
 augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
+--[=====[
+ Returns is cursor on current position surrounded by first and second
+ letter in symbols params
+--]=====]
+local function is_surrounded(symbols)
+  local vim_fn = 'getline(".")[col(".")-2:col(".")-1]'
+
+  return vim_fn .. '=="' .. symbols .. '"'
+end
+
+--[=====[
+  Example:
+
+  if_surrounded({
+    "{}": "bar"
+  }, "b")
+
+  Result:
+  Cursor between braces: {|}. Then press b and result should be: {bar}.
+  In other cases result should be as expected "b"
+
+  CAUTION:
+  By some reason behaviour of this function is unexpected. It is not getting
+  string value of vim maps from string literal (e.g. <Space><Space><Left>)
+  instead function executing this string. So, function is not working
+--]=====]
+local function if_surrounded(table, key_to_remap)
+  local expression = ""
+
+  for symbols, vim_expression in pairs(table) do
+    expression = expression .. is_surrounded(symbols) .. " ? " .. vim_expression .. " : "
+  end
+
+  expression = expression .. key_to_remap
+  return map("i", key_to_remap, expression, true)
+end
+
 return {
   g = g,
   o = o,
   c = c,
   map = map,
   augroup = augroup,
-  autocmd = autocmd
+  autocmd = autocmd,
+  is_surrounded = is_surrounded
+  -- if_surrounded = if_surrounded
 }
