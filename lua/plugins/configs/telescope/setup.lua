5e9051b01cb8eb640490ed460b46344ac7b06ca4
Lothering0 <danil2005232@gmail.com>
2023-03-11 20:35:57 +0300

    Telescope ignore package- and yarn-lock.json files. TODO: make sure that it is works

--- lua/plugins/configs/telescope/setup.lua
+++ lua/plugins/configs/telescope/setup.lua
@@ -1,6 +1,13 @@
 require('telescope').setup{
   defaults = {
-    file_ignore_patterns = { "node_modules", "dist", ".git/" },
+    file_ignore_patterns = {
+      "node_modules",
+      "dist",
+      ".git/",
+      -- TODO: make sure that ignore works for *-lock.json files
+      "^package-lock.json",
+      "^yarn-lock.json",
+    },
     sorting_strategy = "ascending",
     layout_config = {
       horizontal = {
