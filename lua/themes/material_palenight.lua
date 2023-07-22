3f51c706089ed1b5bdafdca3017df110f090954b
Lothering0 <danil2005232@gmail.com>
2023-07-22 17:40:45 +0300

    Palenight theme

---
+++ lua/themes/material_palenight.lua
@@ -0,0 +1,60 @@
+return {
+  meta = {
+    name = "material",
+    style = "palenight"
+  },
+
+  -- Common colors
+  white =    '#EEFFFF',
+  gray =     '#717CB4',
+  black =    '#000000',
+  red =      '#FF406F',
+  green =    '#B9EB80',
+  yellow =   '#F4C056',
+  blue =     '#79A9FF',
+  paleblue = '#B0C9FF',
+  cyan =     '#6CDFFF',
+  purple =   '#BE81DA',
+  orange =   '#FF8663',
+  pink =     '#FF9CAC',
+
+  -- Dark colors
+  darkred =    '#FF406F',
+  darkgreen =  '#B9EB80',
+  darkyellow = '#F4C056',
+  darkblue =   '#79A9FF',
+  darkcyan =   '#6CDFFF',
+  darkpurple = '#BE81DA',
+  darkorange = '#FF8663',
+
+  error =  '#FF406F',
+  link =   '#80CBC4',
+  cursor = '#FF406F',
+  title =  '#EEFFFF',
+
+  none =         'NONE',
+  bg =           '#2F3347',
+  bg_alt =       '#383E54',
+  fg =           '#B8C1D1',
+  text =         '#717CB4',
+  comments =     '#697098',
+  selection =    '#404764',
+  contrast =     '#212433',
+  active =       '#383E54',
+  border =       '#383E54',
+  line_numbers = '#495174',
+  highlight =    '#2F3347',
+  disabled =     '#464B5D',
+  accent =       '#6CDFFF',
+  visual =       '#404764',
+  property =     '#7884EE',
+  operator =     '#BE81DA',
+  punctuation =  '#6CDFFF',
+  ["punctuation.delimiter"] =  '#6CDFFF',
+
+  git = {
+    added = '#B9EB80',
+    modified = '#79A9FF',
+    removed = '#FF406F'
+  }
+}
