8903aad8e306bbce17176f84ef65e0fccef015a0
Lothering0 <danil2005232@gmail.com>
2024-01-27 20:50:41 +0300

    Update Neovide options + Material Forest Theme

--- lua/neovide/options.lua
+++ lua/neovide/options.lua
@@ -4,7 +4,7 @@ local o = helpers.o
 local c = helpers.c
 
 -- Neovide
-c "set guifont=JetBrainsMonoNL\\ Nerd\\ Font:h11"
+c "set guifont=Ubuntu\\ Mono\\ Ligaturized:h13"
 g.neovide_hide_mouse_when_typing = true
 g.neovide_remember_window_size = true
 g.neovide_cursor_trail_size = 0
---
+++ lua/themes/material/forest.lua
@@ -0,0 +1,61 @@
+return {
+  meta = {
+    name = "material",
+    style = "forest"
+  },
+
+  -- Common colors
+  white =    '#EEFFFF',
+  gray =     '#717CB4',
+  black =    '#000000',
+  red =      '#F07178',
+  green =    '#C3E88D',
+  yellow =   '#FFCB6B',
+  blue =     '#82AAFF',
+  paleblue = '#B0C9FF',
+  cyan =     '#74C9DE',
+  purple =   '#A68DCD',
+  orange =   '#CC8868',
+  pink =     '#D3959B',
+
+  -- Dark colors
+  darkred =    '#DC6068',
+  darkgreen =  '#ABCF76',
+  darkyellow = '#FFCB6B',
+  darkblue =   '#6FA0DE',
+  darkcyan =   '#74C9DE',
+  darkpurple = '#A68DCD',
+  darkorange = '#E2795B',
+
+  error =  '#FF5370',
+  link =   '#80CBC4',
+  cursor = '#FFCC00',
+  title =  '#EEFFFF',
+
+  none =         'NONE',
+  bg =           '#151F1D',
+  bg_alt =       '#111917',
+  fg =           '#C2EDD3',
+  text =         '#717CB4',
+  comments =     '#476352',
+  selection =    '#324d3c',
+  contrast =     '#111917',
+  -- #141F1D
+  active =       '#27372d',
+  border =       '#27372d',
+  line_numbers = '#2f4938',
+  highlight =    '#111917',
+  -- disabled =     '#27372d',
+  disabled =     '#314438',
+  accent =       '#84FFFF',
+  visual =       '#324d3c',
+  -- ["@punctuation"] =  '#FFD700',
+  ["@punctuation"] =  '#89DDFF',
+  ["@operator"] =     '#A68DCD',
+
+  git = {
+    added = '#ABCF76',
+    modified = '#6FA0DE',
+    removed = '#DC6068'
+  }
+}
