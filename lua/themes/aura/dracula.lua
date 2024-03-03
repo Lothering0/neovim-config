351c559e1ebcb7f742b09c3d303e5c4eb18f8fda
Lothering0 <danil2005232@gmail.com>
2024-03-03 12:39:24 +0300

    Aura Dracula theme

---
+++ lua/themes/aura/dracula.lua
@@ -0,0 +1,74 @@
+return {
+  meta = {
+    name = "aura",
+    style = "dracula"
+  },
+
+  -- Common colors
+  white =    '#EEFFFF',
+  gray =     '#717CB4',
+  black =    '#000000',
+  red =      '#FF5555',
+  green =    '#68DBAA',
+  yellow =   '#FACE8C',
+  blue =     '#8CD4E4',
+  paleblue = '#8CD4E4',
+  cyan =     '#8CD4E4',
+  purple =   '#936fde',
+  orange =   '#F78C6C',
+  pink =     '#EE8FEC',
+
+  -- Dark colors
+  darkred =    '#FF5555',
+  darkgreen =  '#68DBAA',
+  darkyellow = '#FACE8C',
+  darkblue =   '#8CD4E4',
+  darkcyan =   '#8CD4E4',
+  darkpurple = '#936fde',
+  darkorange = '#E2795B',
+
+  error =  '#FF5555',
+  link =   '#80CBC4',
+  -- cursor = '#868BF5',
+  cursor = '#65C0FC',
+  title =  '#EEFFFF',
+
+  none =         'NONE',
+  bg =           '#15141B',
+  bg_alt =       '#1E1D28',
+  fg =           '#E3E2E9',
+  text =         '#717CB4',
+  comments =     '#524F6F',
+  selection =    '#353446',
+  contrast =     '#1E1D28',
+  active =       '#1E1D28',
+  border =       '#232637',
+  line_numbers = '#2C2640',
+  -- highlight =    '#1F2233',
+  highlight =    '#1F2233',
+  disabled =     '#524F6F',
+  -- accent =       '#936FDE',
+  accent = '#61FFCA',
+  visual =       '#242236',
+  ["@variable.builtin"] = '#936FDE',
+  ["@number"] = '#68DBAA',
+  ["@constant.builtin"] = '#68DBAA',
+  ["@punctuation"] =  '#F8D43F',
+  ["@operator"] =     '#936FDE',
+  ["@keyword.operator"] =     '#936FDE',
+  ["@type"] = '#8CD4E4',
+  ["@type.builtin"] = '#8CD4E4',
+  ["@property"] =     '#EE8FEC',
+  ["@function"] = '#FACE8C',
+  ["@parameter"] = '#E3E2E9',
+  folder = '#61FFCA',
+
+  floating_window              = '#101016',
+  floating_window_current_line = '#1E1D28',
+
+  git = {
+    added = '#68DBAA',
+    modified = '#8CD4E4',
+    removed = '#FF5555'
+  }
+}
